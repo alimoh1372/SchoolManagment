47d9dc2fe76f69e555f0ca66a6800c789f9c3688
Ali Mohammadzadeh <alimohammadzade7976@gmail.com>
2022-03-23 23:09:37 +0430

    14010103-23:08 Adding IBaseRepository

---
+++ SchoolManagment/SchoolManagment.DataLayer/Repositories/IBaseRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using SchoolManagment.DataLayer.DatabaseModel;
+
+namespace SchoolManagment.DataLayer
+{
+    interface IBaseRepository<TEntity> 
+    {
+        TEntity GetById(object id);
+        IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> where = null);
+        void Insert(TEntity entity);
+        void Delete(object id);
+        void Delete(TEntity entity);
+        void Update(TEntity entity);
+
+    }
+}
